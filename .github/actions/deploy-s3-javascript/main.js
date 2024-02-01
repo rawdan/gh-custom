f62eddb9001b32725f353abc1ba0c079ed8f31bd
rawdan <bogdancrudu@yahoo.com>
2024-02-01 14:17:40 +0200

    save js file logic

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,15 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
-    core.notice('hello from my custom javasscript action')
+    // 1) Get some input values
+    core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+    core.notice('hello from my custom JavaScript action!');
+
+    // 2) Upload Files
+    const s3URI = 's3://${bucket}'
+    exec.exec('aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion');
 }
 
 run();
